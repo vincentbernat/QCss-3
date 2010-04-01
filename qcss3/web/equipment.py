a48b07a877bd127a65a56815dcb5b4479a0e36c8
Vincent Bernat <bernat@luffy.cx>
2010-04-01 11:17:59 +0200

    Add "/loadbalancer/XXXX/" handler in web service.

--- qcss3/web/equipment.py
+++ qcss3/web/equipment.py
@@ -22,3 +22,41 @@ class LoadBalancerResource(JsonPage):
                                        "ORDER BY name")
         d.addCallback(lambda x: [y[0] for y in x])
         return d
+
+    def childFactory(self, ctx, name):
+        return LoadBalancerDetailResource(name, self.dbpool, self.collector)
+
+class LoadBalancerDetailResource(JsonPage):
+    """
+    Return details about a load balancer.
+
+    For example::
+      {"name": "loadbalancer1.example.com",
+       "description": "Nortel Application Switch 2208",
+       "type": "AAS"}
+    """
+
+    def __init__(self, name, dbpool, collector):
+        self.name = name
+        self.dbpool = dbpool
+        self.collector = collector
+        JsonPage.__init__(self)
+
+    def data_json(self, ctx, data):
+        d = self.dbpool.runQueryInPast(ctx, """
+SELECT name, description, type
+FROM loadbalancer
+WHERE name=%(name)s
+AND deleted='infinity'
+""",
+                                       {'name': self.name})
+        d.addCallback(self.format_json)
+        return d
+
+    def format_json(self, data):
+        if not data:
+            # Not found, 404
+            return None
+        return {'name': data[0][0],
+                'description': data[0][1],
+                'type': data[0][2]}
